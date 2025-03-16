03a909faa78a38be85290bc9ebc1202cf787767d
Ander <volintine_21001524@utp.edu.my>
2025-03-16 20:18:10 +0800

    Prepare locally

--- prepare.py
+++ prepare.py
@@ -3,12 +3,7 @@ import requests
 import tiktoken
 import numpy as np
 
-# download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w', encoding='utf-8') as f:
-        f.write(requests.get(data_url).text)
+input_file_path = "./tilo_pomoluda.txt"
 
 with open(input_file_path, 'r', encoding='utf-8') as f:
     data = f.read()
@@ -28,6 +23,3 @@ train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
-
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
